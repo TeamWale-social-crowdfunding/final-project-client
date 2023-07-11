a2e89d4bb17acfd1d9a3c4525621cf43e8d77a33
quocld <quocle2208@gmail.com>
2023-07-12 00:00:11 +0700

    feat: message footer ui

---
+++ src/pages/messages/components/MessageTime.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageTime = () => {
+  return (
+    <p className="p-4 text-center text-sm text-gray-500 dark:text-white">
+      12:40 PM
+    </p>
+  );
+};
+
+export default MessageTime;
--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -2,8 +2,10 @@ import HeaderOnly from "@/src/layouts/HeaderOnly/HeaderOnly";
 import MessageLayout from "@/src/layouts/MessageLayout/MessageLayout";
 import React from "react";
 import MessageBox from "./components/MessageBox";
+import MessageChatFooter from "./components/MessageChatFooter";
 import MessageGroupRecive from "./components/MessageGroupRecive";
 import MessageGroupSent from "./components/MessageGroupSent";
+import MessageTime from "./components/MessageTime";
 
 const Messages = () => {
   return (
@@ -11,7 +13,10 @@ const Messages = () => {
       <MessageBox>
         <div className=" relative w-full h-16"></div>
         <MessageGroupRecive></MessageGroupRecive>
+        <MessageTime></MessageTime>
         <MessageGroupSent></MessageGroupSent>
+        <div className=" relative w-full h-16"></div>
+        <MessageChatFooter></MessageChatFooter>
       </MessageBox>
     </MessageLayout>
   );
