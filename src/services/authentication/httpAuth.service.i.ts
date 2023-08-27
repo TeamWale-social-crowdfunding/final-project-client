f5d2c2a12f4f6a9fe6b33c63c0e6c7d9f274c7b8
quocld <quocle2208@gmail.com>
2023-08-27 21:26:07 +0700

    ✨ feat(auth-service): google login

---
+++ src/services/authentication/httpAuth.service.i.ts
@@ -0,0 +1,11 @@
+export interface GoogleAuthDto {
+  email: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  googleId: string;
+
+  avatar: string;
+}
--- src/services/authentication/httpAuth.service.ts
+++ src/services/authentication/httpAuth.service.ts
@@ -1,9 +1,15 @@
 import HttpService from "../http.service";
+import { GoogleAuthDto } from "./httpAuth.service.i";
 
 export class AuthHttpService extends HttpService {
   constructor() {
     super("http://localhost:3001");
   }
+
+  googleAuth(data: GoogleAuthDto) {
+    return this.post("auth/google-auth", { data }, { withCredentials: true });
+  }
+
   getCurrentUser() {
     return this.get("/auth/users/current", { withCredentials: true });
   }
